544994586f612cfabfb1a0821aacb0d0cbe72ad9
VolchkovIlia <113889753+VolchkovIlia@users.noreply.github.com>
2023-03-04 19:30:09 +0300

    Add files via upload

---
+++ Lr 8/Lr 7/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr_7
+{
+    public  class Class1
+    {
+        public static long Factorial(int n)
+        {
+            long p = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                p = p * i;
+            }
+            return p;
+        }
+    }
+}
